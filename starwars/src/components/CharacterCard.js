883b49f3358db96a9b58c40e817c9531a77e7dcf
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-03-20 12:12:52 -0400

    Created components that loop through and output data

---
+++ starwars/src/components/CharacterCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    Card, CardTitle, CardText, CardBody, Col, Container
+} from 'reactstrap';
+import styled from 'styled-components';
+
+const CharName = styled.h1`
+    border: 1px solid white;
+
+`
+
+
+
+const CharacterCard = props => {
+    return (
+        <div>
+            
+            
+            <Container >
+            <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+                
+                <CardBody sm="6">
+                    {/* <CardTitle>Name: {props.content.name}</CardTitle> */}
+                    <CharName>{props.content.name}</CharName>
+                    <CardText>Height: {props.content.height}</CardText>
+                    <CardText>Birth Year: {props.content.birth_year}</CardText>
+                    <CardText>Gender: {props.content.gender}</CardText>
+                </CardBody>
+                
+            </Card>
+            </Container>
+            
+            
+        </div>
+    )
+}
+
+export default CharacterCard;
